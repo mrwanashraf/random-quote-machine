6770e9f9a1e2d39aa895e2e59f5e903481da727a
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-04-22 22:46:54 +0200

    change deploy path along with its related files

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: path.resolve(__dirname, './'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js'
     },
     resolve: {
         modules: [path.resolve(__dirname, 'src'), 'node_modules']
